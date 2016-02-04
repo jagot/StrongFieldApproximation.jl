d5d25dc37fb5c66241ebc44da4cd24631442c4fe
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-02-04 14:22:55 +0100

    Parallelized propagation

--- src/propagate.jl
+++ src/propagate.jl
@@ -1,4 +1,3 @@
-using ProgressMeter
 using SFA.trapz
 
 #=
@@ -32,6 +31,36 @@ with \(\tau\equiv t-t'\), \(S_{\textrm{st}}(t,\tau)\equiv S(p_{\textrm{st}},t,t-
 \[p_{\textrm{st}}(t,\tau)=\frac{x(t)-x(t-\tau)}{\tau} = \frac{1}{\tau}\int\limits_{t-\tau}^t dt' A(t').\]
 =#
 
+function calc_i(i, offset, δt, Av, Ev, Ip, d)
+    τ = reverse(1:i+offset)*δt
+    nτ = length(τ)
+
+    At = Av[i+offset]
+
+    pτ = 0
+    dx = 0im
+
+    for j = nτ:-1:1
+        ζ = (π/(1e-10 + im*(τ[j]-δt)/2))^(3/2)
+
+        AtP = Av[j]
+        EtP = Ev[j]
+        pτ += AtP
+        p = pτ*δt/τ[j] # Saddle-point momentum
+
+        pion = p - AtP
+        prec = p - At
+
+        dion = d(pion)
+        drec = d(prec)
+
+        S = trapzv(0.5(p - Av[j:i+offset-1]).^2 + Ip, δt) # Saddle-point action
+
+        dx += ζ*conj(drec)*exp(-im*S)*EtP*dion
+    end
+    -imag(dx)
+end
+
 # tlims set the limits of the integration: t will assume values in the
 # interval [tlims[1],tlims[2]] with ndt steps per cycle. t' will
 # assume values in the interval [tlims[3],t], ∀ t, tlims[3] ⩾ 0, else
@@ -42,47 +71,23 @@ function propagate(A,E,Ip,d,tlims,T,ndt)
     t = (tlims[1]*ndt+1:tlims[2]*ndt)*δt
     offset = (tlims[1]-tlims[3])*ndt-1
     nt = length(t)
-    t2 = (2:nt+offset+1)*δt
     tlims = collect(tlims)*T
 
-    Av = A(t2)
-    Ev = E(t2)
 
-    x = zeros(nt)
-
-    @showprogress for i = nt:-1:1
-        τ = if tlims[3] < 0
-            [1]*δt # Not implemented yet
-        else
-            reverse(1:i+offset)*δt
-        end
-        nτ = length(τ)
-
-        At = Av[i+offset]
-
-        pτ = 0
-        dx = 0im
-
-        for j = nτ:-1:1
-            ζ = (π/(1e-10 + im*(τ[j]-δt)/2))^(3/2)
-
-            AtP = Av[j]
-            EtP = Ev[j]
-            pτ += AtP
-            p = pτ*δt/τ[j] # Saddle-point momentum
-
-            pion = p - AtP
-            prec = p - At
-
-            dion = d(pion)
-            drec = d(prec)
-
-            S = trapzv(0.5(p - Av[j:i+offset-1]).^2 + Ip, δt) # Saddle-point action
-
-            dx += ζ*conj(drec)*exp(-im*S)*EtP*dion
-        end
-        x[i] = -imag(dx)
+    t2 = SharedArray(Float64, nt+offset)
+    Av = SharedArray(Float64, nt+offset)
+    Ev = SharedArray(Float64, nt+offset)
+    @parallel for i = 1:nt+offset
+        tt = (i+1)*δt
+        t2[i] = tt
+        Av[i] = A(tt)
+        Ev[i] = E(tt)
     end
+
+    x = sdata(SharedArray(Float64, nt,
+                          init = S -> (S[Base.localindexes(S)] =
+                                      map(i -> calc_i(i, offset, δt, Av, Ev, Ip, d),
+                                          Base.localindexes(S)))))
     x *= 2δt
 
     x,t
