154d25e248b24bf5dd00effc61b26519a54bc706
Stefanos Carlström <stefanos.carlstrom@fysik.lth.se>
2016-11-07 15:55:20 +0100

    Optional soft window used to define integral limits

--- src/propagate.jl
+++ src/propagate.jl
@@ -31,22 +31,29 @@ with \(\tau\equiv t-t'\), \(S_{\textrm{st}}(t,\tau)\equiv S(p_{\textrm{st}},t,t-
 \[p_{\textrm{st}}(t,\tau)=\frac{x(t)-x(t-\tau)}{\tau} = \frac{1}{\tau}\int\limits_{t-\tau}^t dt' A(t').\]
 =#
 
-function calc_i(i, offset, jmin, δt, Av, Ev, Ip, d)
-    τ = reverse(1:i+offset)*δt
+function calc_i(i, j, δt, Av, Ev, Ip, d, window)
+    # By reversing the τ vector, we make sure that the smallest
+    # excursion times are paired with the latest ionization events (t'
+    # close to t).
+    τ = reverse(1+j-j[1])*δt
     nτ = length(τ)
 
-    At = Av[i+offset]
+    At = Av[j[end]]
 
     pτ = 0
     dx = 0im
 
-    for j = nτ:-1:jmin
-        ζ = (π/(1e-10 + im*(τ[j]-δt)/2))^(3/2)
+    # The loop goes from the latest ionization event before t until
+    # the earliest.
+    for jj = reverse(1:length(j))
+        ζ = (π/(1e-10 + im*(τ[jj]-δt)/2))^(3/2)
 
-        AtP = Av[j]
-        EtP = Ev[j]
+        jjj = j[jj]
+
+        AtP = Av[jjj]
+        EtP = Ev[jjj]
         pτ += AtP
-        p = pτ*δt/τ[j] # Saddle-point momentum
+        p = pτ*δt/τ[jj] # Saddle-point momentum
 
         pion = p - AtP
         prec = p - At
@@ -54,74 +61,69 @@ function calc_i(i, offset, jmin, δt, Av, Ev, Ip, d)
         dion = d(pion)
         drec = d(prec)
 
-        S = trapzv(0.5(p - Av[j:i+offset-1]).^2 + Ip, δt) # Saddle-point action
+        S = trapzv(0.5(p - Av[jjj:j[end]]).^2 + Ip, δt) # Saddle-point action
 
-        dx += ζ*conj(drec)*exp(-im*S)*EtP*dion
+        dx += ζ*conj(drec)*exp(-im*S)*EtP*dion*window(i,jjj)[1]
     end
     -imag(dx)
 end
 
-# tlims set the limits of the integration: t will assume values in the
-# interval [tlims[1],tlims[2]] with ndt steps per cycle. t' will
-# assume values in the interval [tlims[3],t], ∀ t, tlims[3] ⩾ 0, else
-# [t - tlims[3],t].
-function propagate(A::Function,E::Function,Ip::Real,d::Function,tlims,T::Real,ndt::Integer)
-    δt = T/ndt
-
-    t = (tlims[1]*ndt+1:tlims[2]*ndt)*δt
-    if tlims[3]>=0
-        offset = (tlims[1]-tlims[3])*ndt-1
-        jmin = 1
+function limits(tmin,tmax,τmax,ndt,windowed=false)
+    i = 1:(tmax-tmin)*ndt # Index vector of output
+    if τmax == 0
+        τmax2 = 0
+        j = i -> 1:i
+        window = (i,jj) -> ones(Float64,length(jj))
+    elseif τmax < 0
+        τmax2 = τmax - (windowed ? 0.35 : 0)
+        jmin = round(Int, abs(τmax2)*ndt)
+        j = i -> i + (0:jmin-1)
+        window = if !windowed
+            (i,jj) -> ones(Float64,length(jj))
+        else
+            # Optional, soft window function used when selecting excursion
+            # times < abs(τmax).
+            w = j -> (j .< 0) + (1.0 - exp(-((j/ndt-1).^6/0.001)).^4) .* ((0.<=j) .* (j.<ndt))
+            (i,jj) -> w(ndt+i-jj)
+        end
     else
-        offset = tlims[1]*ndt - 1
-        jmin = round(Int, offset + tlims[3]*ndt)
+        error("Invalid τmax, $(τmax)")
     end
-    nt = length(t)
-    tlims = collect(tlims)*T
+    # Real time vector, x will be calculated at these points.
+    t = (tmin*ndt+1:tmax*ndt)/ndt
+    # Time vector, including excursions before tmin, in the case of
+    # τmax ≠ 0. Fields are to be evaluated at these points.
+    tt = ((tmin+τmax2)*ndt+1:tmax*ndt)/ndt
 
-    t2 = SharedArray(Float64, nt+offset)
-    Av = SharedArray(Float64, nt+offset)
-    Ev = SharedArray(Float64, nt+offset)
-    @parallel for i = 1:nt+offset
-        tt = (i+1)*δt
-        t2[i] = tt
-        Av[i] = A(tt)
-        Ev[i] = E(tt)
-    end
 
-    x = sdata(SharedArray(Float64, nt,
-                          init = S -> (S[Base.localindexes(S)] =
-                                      map(i -> calc_i(i, offset, jmin + i, δt, Av, Ev, Ip, d),
-                                          Base.localindexes(S)))))
-    x *= 2δt
-
-    x,t
+    i,j,t,tt,window
 end
 
-function propagate(t::AbstractVector,
-                   A::Vector,E::Vector,
+# tlims set the limits of the integration: t will assume values in the
+# interval [tlims[1],tlims[2]] with ndt steps per cycle. t' will
+# assume values in the interval [t - |tlims[3]|,t].
+function propagate(A::Function,E::Function,
                    Ip::Real,d::Function,
-                   T::Real,ndt::Integer,
-                   tmin::Real = -0.65)
+                   tlims,T::Real,ndt::Integer;
+                   windowed = false)
+    δt = T/ndt
+    i,j,t,tt,window = limits(tlims...,ndt,windowed)
     nt = length(t)
+    ntt = length(tt)
 
-    Av = SharedArray(Float64, nt)
-    Ev = SharedArray(Float64, nt)
-    @parallel for i = 1:nt
-        Av[i] = A[i]
-        Ev[i] = E[i]
+    Av = Array(Float64, ntt)
+    Ev = Array(Float64, ntt)
+    for (i,ttt) in enumerate(tt)
+        Av[i] = A(ttt*ndt*δt)
+        Ev[i] = E(ttt*ndt*δt)
     end
 
-    δt = T/ndt
-    jmin = round(Int, tmin*ndt)
+    x = Array(Float64, nt)
+    for i in eachindex(t)
+        x[i] = calc_i(i, j(i), δt, Av, Ev, Ip, d, window)
+    end
 
-    x = sdata(SharedArray(Float64, nt,
-                           init = S -> (S[Base.localindexes(S)] =
-                                       map(i -> SFA.calc_i(i, 0, max(jmin+i, 1), δt, Av, Ev, Ip, d),
-                                           Base.localindexes(S)))))
-
-    x *= 2δt
-    x
+    x*2δt,t*ndt*δt
 end
 
 export propagate
